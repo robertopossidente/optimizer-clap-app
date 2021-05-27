2af6af438c2e603ba95a7964aec47f23a45dd6ef
Otavio Napoli <>
2021-05-27 10:37:32 -0300

    Added README instructions and code comments

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # optimizer-clap-app
+
+Optimizer clapp app é uma aplicação para o CLAP que visa otimizar a execução de 
+aplicação em um aglomerado dinamicamente. Estes repositório é um **modelo** e pode 
+ser alterado como desejado.
+
+Para usar este app, coloque o arquivo `optimizer.py` no diretório onde o CLAP foi 
+instalado (feito o git clone) dentro de `app/cli/modules/`.
+
+Com isso você consegue utilizar esta aplicação diretamente pela linha de comando 
+do CLAP. Esta aplicação pode ser chamada utilizando o comando `clapp optimizer run`.
+Esta aplicação recebe os seguintes parametros:
+* `--cluster-id`: Identificador de um Cluster no CLAP executando a aplicação.
+* `--experiment-id`: Nome único do experimento.
+* `-vm-price`: Arquivo YAML contendo o preço por hora das VMs (veja o arquivo de exemplo `vm_prices.yaml`)
+* `--root-dir` (opcional): Direrótio raiz para salvar informações do experimentos. Irá criar os seguintes diretórios: `<root_dir>/<expriment_id>/app_results`, `<root_dir>/<expriment_id>/optimizer_logs` and `<root_dir>/<expriment_id>/PIs_logs`.
+* `--report-time` (opcional): Tempo de espera antes para executar o reporter e o otimizador.
+
+Ao executar a aplicação através do comando `clapp optimizer run` a função `optimizer_run` será executada como ponto de entrada. 
+
--- optimizer.py
+++ optimizer.py
@@ -22,6 +22,7 @@ cluster_manager = get_cluster_manager()
 
 
 # Class Reporter
+# You must implement these 3 methods as speficied in the document
 class Reporter:
     def get_metrics(self, cluster_id: str, experiment_id: str,
                     pi_logs_dir: str, instance_costs: Dict[str, float]) -> \
@@ -37,14 +38,17 @@ class Reporter:
 
 
 # Class Optimizer
+# You must implement one method as speficied in the document
 class Optimizer:
     def run(self, cluster_id: str, experiment_id: str,
             metrics: Dict[str, float]) -> bool:
         pass
 
 
+# Function used to dynamic optimization
 def optimize_it(cluster_id: str, experiment_id: str, vm_price_file: str,
                 root_dir: str, report_time: int = 60) -> int:
+    # Create experiments directory
     experiment_dir = path_extend(root_dir, experiment_id, str(int(time.time())))
     app_results_dir = path_extend(experiment_dir, 'app_results')
     optimizer_logs_dir = path_extend(experiment_dir, 'optimizer_logs')
@@ -53,24 +57,39 @@ def optimize_it(cluster_id: str, experiment_id: str, vm_price_file: str,
     os.makedirs(optimizer_logs_dir, exist_ok=True)
     os.makedirs(pis_logs_dir, exist_ok=True)
 
+    # Print some information...
     print(f"Cluster ID: {cluster_id}, Experiment ID: {experiment_id}, "
           f"VM Price File: {vm_price_file}, APP Result Dir: {app_results_dir}, "
           f"Optimizer Logs Dir: {optimizer_logs_dir}, PI Logs Dir: {pis_logs_dir}")
+    
+    # Get reporter and optimizer objects
     reporter_obj = Reporter()
     optimizer_obj = Optimizer()
+    # Read VM prices from vm_price_file. The result is a dictionary
     prices = yaml_load(vm_price_file)
 
+    # You may want to print nodes init time here....
+    # .....
+
+    # Continue until application terminates
     while True:
+	# Sleep for report_time seconds
         time.sleep(report_time)
+        # Check if the application already terminated.
         if reporter_obj.terminated(cluster_id, experiment_id):
+            # Fetch results and terminate cluster
             reporter_obj.fetch_results(cluster_id, experiment_id, app_results_dir)
             cluster_manager.stop_cluster(cluster_id)
             return 0
         else:
+            # Get cost for nodes
             metrics = reporter_obj.get_metrics(
                 cluster_id, experiment_id, pis_logs_dir, prices)
+            # Optimize it!            
             changed = optimizer_obj.run(cluster_id, experiment_id, metrics)
             print(f"Does cluster changed? {changed}")
+    # This should never be reached...    
+    return 1
 
 
 # Command-line interface
@@ -90,12 +109,13 @@ def optimizer():
 @click.option('-v', '--vm-price', default=None, show_default=False,
               type=str, required=True,
               help='Path to the YAML file with the price of the VMs')
-@click.option('-r', '--root-dir', default=None, show_default=False,
-              type=str, required=True,
+@click.option('-r', '--root-dir', default='.', show_default=False,
+              type=str, required=False,
               help='Root directory where experiment directories will be created')
 @click.option('-rt', '--report-time', default=60, show_default=True,
-              type=int, required=True,
+              type=int, required=False,
               help='Time to wait before calling reporter')
 def optimizer_run(cluster_id: str, experiment_id: str, vm_price: str,
                   root_dir: str, report_time: int):
     return optimize_it(cluster_id, experiment_id, vm_price, root_dir, report_time)
+
